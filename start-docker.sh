5bf04e8ba838026b28d961f2550a575357be5c2a
Fernando Cagua <efcaguab@gmail.com>
2018-03-26 16:06:12 +1300

    update shiny to install stuff missing

--- start-docker.sh
+++ start-docker.sh
@@ -1,4 +1,9 @@
-docker run -d -p 80:3838 \
-    -v ~/github/data-rich-insight-poor/apps:/srv/shiny-server/ \
-    -v ~/github/data-rich-insight-poor/logs:/var/log/shiny-server/ \
-    efcaguab/shiny
+docker run --name mywebsite -d -p 80:3838 \
+    -v ~/github/cagua_com/apps:/srv/shiny-server/ \
+    -v ~/github/cagua_com/logs:/var/log/shiny-server/ \
+    rocker/shiny
+
+docker exec -it mywebsite apt-get update
+docker exec -it mywebsite sudo apt-get install -y libxml2-dev libssl-dev
+docker exec -it mywebsite Rscript -e "install.packages('packrat')"
+docker exec -it mywebsite Rscript -e "packrat::restore('/srv/shiny-server/demografia-del-voto')"
