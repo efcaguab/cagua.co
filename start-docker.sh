c5d564f3dd0c09ceab3093d65c849e86a7e9d660
Fernando Cagua <efcaguab@gmail.com>
2018-03-30 20:50:26 +1300

    Update directory

--- start-docker.sh
+++ start-docker.sh
@@ -1,11 +1,13 @@
 docker run --name mywebsite -d -p 80:3838 \
-    -v ~/github/cagua_com/apps:/srv/shiny-server/ \
-    -v ~/github/cagua_com/logs:/var/log/shiny-server/ \
+    -v ~/github/cagua-website/cagua_com/apps:/srv/shiny-server/ \
+    -v ~/github/cagua-website/cagua_com/logs:/var/log/shiny-server/ \
     rocker/shiny
 
 docker exec -it mywebsite apt-get update
+
+# demografia del voto app
+docker exec -it mywebsite Rscript -e "install.packages('packrat')"
 docker exec -it mywebsite sudo apt-get install -y libxml2-dev libssl-dev
 docker exec -it mywebsite sudo chown -R shiny:shiny /srv/shiny-server/demografia-del-voto
-docker exec -it mywebsite Rscript -e "install.packages('packrat')"
 docker exec -it mywebsite Rscript -e "packrat::restore('/srv/shiny-server/demografia-del-voto')"
 docker exec -it mywebsite make -C /srv/shiny-server/demografia-del-voto
