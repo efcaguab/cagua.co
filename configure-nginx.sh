4a0d1c8661a349174938e0cf65106640ffbfc580
Fernando Cagua <efcaguab@gmail.com>
2018-04-01 07:56:20 +1300

    Copy nginx config properly

--- configure-nginx.sh
+++ configure-nginx.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cp ./nginx-full.conf /usr/local/etc/nginx/nginx.conf
+cp ./nginx-full.conf /etc/nginx/nginx.conf
 rm -r /var/www/html
 mkdir /var/www
 ln -s ~/github/cagua-website/cagua_com/public_html /var/www/html
