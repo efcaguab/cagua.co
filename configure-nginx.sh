af385836d4d8fd89c19ddbed9f272e0cd6c74479
Fernando Cagua <efcaguab@gmail.com>
2018-04-02 21:26:14 +1200

    nginx file in default location DO

--- configure-nginx.sh
+++ configure-nginx.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+
 cp ./nginx-full.conf /usr/local/etc/nginx/nginx.conf
 cp ./nginx-full.conf /etc/nginx/nginx.conf
+mv /etc/nginx/sites-enabled/default /etc/nginx/sites-enabled/default-copy 
+cp ./nginx-full.conf /etc/nginx/sites-enabled/default
+
 rm -r /var/www/html
 mkdir /var/www
 ln -s ~/github/cagua-website/cagua_com/public_html /var/www/html
