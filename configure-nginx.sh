ad3824c159b8e54e7976a0927edc44d01626ae3e
Fernando Cagua <efcaguab@gmail.com>
2018-04-02 22:39:15 +1200

    Update ngingx configuration for DO

--- configure-nginx.sh
+++ configure-nginx.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-
-cp ./nginx-full.conf /usr/local/etc/nginx/nginx.conf
-cp ./nginx-full.conf /etc/nginx/nginx.conf
-mv /etc/nginx/sites-enabled/default /etc/nginx/sites-enabled/default-copy 
-cp ./nginx-full.conf /etc/nginx/sites-enabled/default
+cp ./nginx-full.conf /etc/nginx/sites-available/cagua
+ln -s /etc/nginx/sites-available/cagua /etc/nginx/sites-enabled/cagua
 
 rm -r /var/www/html
 mkdir /var/www
