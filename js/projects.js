4ef2389e347b027869aeab6a4698fb88dc8e712a
Fernando Cagua <efcaguab@gmail.com>
2018-04-20 00:38:58 +1200

    Initial js commit of project

---
+++ js/projects.js
@@ -0,0 +1,17 @@
+var projects = [
+  {
+    "id": "pollination",
+    "name": "Pollination ecology",
+    "description": "Pollination is a very special thing we like and depend on. It's also special because species helping each other. However we don't understand this and that. Therefore I'm investigating the factors that affect the stability of pollination  ",
+    "image":"bee"
+  },
+  {
+    "id": "network",
+    "name": "Network Science",
+    "description": ""
+  }, 
+  {
+    "id": "ws",
+    "name": "Whale Sharks"
+  }
+]
