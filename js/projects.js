926eacb8542a816cfb744ab9075edf091e0c3293
Fernando Cagua <efcaguab@gmail.com>
2018-04-20 23:30:41 +1200

    Create divs with card headers

--- js/projects.js
+++ js/projects.js
@@ -2,16 +2,67 @@ var projects = [
   {
     "id": "pollination",
     "name": "Pollination ecology",
-    "description": "Pollination is a very special thing we like and depend on. It's also special because species helping each other. However we don't understand this and that. Therefore I'm investigating the factors that affect the stability of pollination  ",
+    "description": "Animal pollination is key for global crop producction and the maintenance of biodiversity. Pollination is supported by animal interactions of a special kind. One in which both plants and pollinators benefit from each other. of mutual benefitThe interactions that support pollination, those between animals and plants, are very special because pollination a very special thing we like and depend on. It's also special because species helping each other. However we don't understand this and that. Therefore I'm investigating the factors that affect the stability of pollination  ",
     "image":"bee"
   },
   {
     "id": "network",
     "name": "Network Science",
-    "description": ""
+    "description": "asdasda",
+    "image": "net"
   }, 
   {
     "id": "ws",
-    "name": "Whale Sharks"
+    "name": "Whale Sharks",
+    "description": "asdas",
+    "image": "fin"
   }
 ]
+
+createCardHeaderProjects = function (item, targetID, headerID) {
+  var linkDiv = document.createElement("a")
+  linkDiv.setAttribute("data-toggle", "collapse")
+  linkDiv.setAttribute("data-target", "#" + targetID)
+  linkDiv.setAttribute("aria-expanded", "false")
+  linkDiv.setAttribute("aria-controls", targetID)
+  linkDiv.setAttribute("href", "#!")
+  var linkTitle = document.createElement("h4")
+  linkTitle.classList.add("project-element-title")
+  linkTitle.innerHTML = item.name
+  var badgeDiv = document.createElement("img")
+  badgeDiv.setAttribute("src", "media/" + item.image + ".svg")
+  badgeDiv.setAttribute("alt", item.image)
+  linkDiv.appendChild(badgeDiv)
+  linkDiv.appendChild(linkTitle)
+  var cardHeaderDiv = document.createElement("div")
+  cardHeaderDiv.classList.add("card-header")
+  cardHeaderDiv.classList.add("card-header-resume")
+  cardHeaderDiv.classList.add("py-2")
+  cardHeaderDiv.id = headerID
+  cardHeaderDiv.appendChild(linkDiv)
+  return cardHeaderDiv
+}
+
+createCardProjects = function (item, parent) {
+  var targetID = "collapse-proj-" + item.id
+  var headerID = "header-proj-" + item.id
+
+  var cardDiv = document.createElement("div")
+  cardDiv.classList.add("card")
+  cardDiv.classList.add("card-projects")
+  cardDiv.appendChild(createCardHeaderProjects(item, targetID, headerID))
+  // cardDiv.appendChild(createCardCollapse(item, targetID, headerID, parent))
+  return cardDiv
+}
+
+
+createAcordionProjects = function (items, acordionID) {
+  var acordeonDiv = document.createElement("div")
+  var parentID = "acordion-" + acordionID
+  acordeonDiv.classList.add("acordion")
+  acordeonDiv.id = parentID
+  for (var i = 0; i < items.length; i++) {
+    acordeonDiv.appendChild(createCardProjects(items[i], parentID))
+  }
+  return acordeonDiv
+}
